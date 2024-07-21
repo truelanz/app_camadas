b040241812e1c8351c921aa26752a673a0377870
truelanz <121499741+truelanz@users.noreply.github.com>
2024-07-21 20:01:07 -0300

    Add project files.

---
+++ DataAccessLayer/ConnectionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataAccessLayer
+{
+    public class ConnectionData
+    {
+        public static String StringConnection
+        {
+            get
+            {
+                return "Server = localhost; Database = crud; Uid = truelanz";
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/DataAccesslayerConnection.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace DataAccessLayer
+{
+    public class DataAccesslayerConnection
+    {
+        private string _stringConnection;
+        private MySqlConnection _connection;
+
+        public DataAccesslayerConnection(string connectionData)
+        {
+            this._connection = new MySqlConnection();
+            this._stringConnection = connectionData;
+            this._connection.ConnectionString = connectionData;
+        }
+
+        public MySqlConnection ConnectionObj
+        {
+            get { return this._connection; }
+            set { this._connection = value; }
+        }
+
+        public string ConnectionString
+        {
+            get { return this._stringConnection; }
+            set { this._stringConnection = value; }
+        }
+
+        public void Connect()
+        {
+            this._connection.Open();
+        }
+
+        public void Close()
+        {
+            this._connection.Close();
+        }
+    }
+}
